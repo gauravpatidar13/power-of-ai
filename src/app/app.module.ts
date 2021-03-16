15ab0c383e41a9262f846b6e26f1b9ec15285128
Gaurav patidar <gp26101998@gmail.com>
2021-03-16 17:01:29 +0530

    first adio design template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ClassifiedComponent } from './classified/classified.component';
+import { ReviewComponent } from './review/review.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    ClassifiedComponent,
+    ReviewComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
